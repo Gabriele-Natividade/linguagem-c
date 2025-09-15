0f03bec10e89b9943790404a679e426de41f52d3
Gabriele-Natividade <natividadegabriele438@gmail.com>
2025-09-15 20:52:57 -0300

    add ex2 file

---
+++ condicionais/ex2.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 379;
+    int c, d, u, resultado;
+
+    c = n / 100;                // pega a CENTENA ? 3
+    d = (n % 100) / 10;         // pega a DEZENA  ? 7
+    u = n % 10;                 
+
+    resultado = (u * 100) + (d * 10) + c;   // inverte os valores
+
+    printf("valor inicial: %d \n", n);
+    printf("valor invertido: %d \n", resultado);
+
+    return 0;
+}
