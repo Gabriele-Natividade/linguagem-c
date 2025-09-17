a2ebcbb8f43745c821f3d7af3a3feab36090245f
Gabriele-Natividade <natividadegabriele438@gmail.com>
2025-09-16 21:25:16 -0300

    add calculadora.c

---
+++ condicionais/calculadora.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(void) {
+    float x, y, resultado;
+    int op, continuar;
+
+    do {
+        printf("\n\t1 - soma\n\t2 - subtracao\n\t3 - multiplicacao\n\t4 - divisao");
+        printf("\nDigite a opcao desejada: ");
+        scanf("%d", &op);
+
+        printf("\nDigite o primeiro numero: ");
+        scanf("%f", &x);
+
+        printf("\nDigite o segundo numero: ");
+        scanf("%f", &y);
+
+        switch(op) {
+            case 1:
+                resultado = x + y;
+                printf("\nO resultado da soma e: %f", resultado);
+                break;
+
+            case 2:
+                resultado = x - y;
+                printf("\nO resultado da subtracao e: %f", resultado);
+                break;
+
+            case 3:
+                resultado = x * y;
+                printf("\nO resultado da multiplicacao e: %f", resultado);
+                break;
+
+            case 4:
+                if (y != 0) {
+                    resultado = x / y;
+                    printf("\nO resultado da divisao e: %f", resultado);
+                } else {
+                    printf("\nErro: divisao por zero!");
+                }
+                break;
+
+            default:
+                printf("\nOperacao invalida");
+                break;
+        }
+
+        printf("\n\nDeseja continuar calculando? (1-sim / 0-nao): ");
+        scanf("%d", &continuar);
+
+    } while(continuar != 0);
+
+    return 0;
+}
