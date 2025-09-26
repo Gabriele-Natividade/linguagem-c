69b6d9100635239735ffe0a11b9aa136900e1fc9
Gabriele-Natividade <natividadegabriele438@gmail.com>
2025-09-25 21:37:23 -0300

    Refactor code structure for improved readability and maintainability

---
+++ Repetição/ex.002.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int alturaA, alturaB;
+    int anos=0;
+
+    printf("/n Digite a altura da pessoa A em centimetros: ");
+    scanf("%d", &alturaA);
+    
+    printf("/n Digite a altura da pessoa Bem centimetros: ");
+    scanf("%d", &alturaB);
+
+    while(alturaB <= alturaA) {
+        alturaA +=2;
+        alturaB +=3;
+
+        anos++;
+    }
+       
+    printf("/n Serao necessarios %d anos para a pessoa B ser maior que a pessoa A.");
+    return 0;
+} 
